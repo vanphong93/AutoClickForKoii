e4162d263b291a15fa6cd7bf2da371fdf91e99e6
Van Phong <phongckm93@gmail.com>
2024-04-09 07:14:50 +0700

    update image

--- tool.py
+++ tool.py
@@ -2,7 +2,7 @@
 import pyautogui as pag
 import time
 #Number of task  you want to run
-number=4
+number=3
 # Poin X (head)
 x=326
 # Poin Y (head)
@@ -26,30 +26,37 @@ while True:
     if isDoing:
         print("Koii is running")
         for pointY in range(y, y + distance*number,distance):
-                pag.moveTo(x,pointY)
+                # pag.moveTo(x,pointY)
                 time.sleep(6)
-                result=pag.pixelMatchesColor(x,pointY,(255,255,255))
-                if result:
-                    pag.leftClick(x,pointY)
-    else :
-        if autoRestart:
-            print("Some thing wrong, app is restarting")
-            try:
-                loc=pag.locateOnScreen("IconApp.png")
-                pag.doubleClick(loc)
-                time.sleep(25)
-                pag.press(myPass[0])
-                time.sleep(2)
-                pag.press(myPass[1])
-                time.sleep(2)
-                pag.press(myPass[2])
-                time.sleep(2)
-                pag.press(myPass[3])
-                time.sleep(2)
-                pag.press(myPass[4])
-                time.sleep(2)
-                pag.press(myPass[5])
-                time.sleep(20)
-            except pag.ImageNotFoundException:
-                print("Sorry, Image cannot be recognized")
-    time.sleep(timeCheck)
+                try:
+                    start = pag.locateOnScreen('play1.png',grayscale=True,confidence=0.8)
+                    pag.click(start)
+                    print("I see")
+                except pag.ImageNotFoundException:
+                     pass
+                     
+                # result=pag.pixelMatchesColor(x,pointY,(255,255,255))
+                # if result:
+                #     pag.leftClick(x,pointY)
+    # else :
+    #     if autoRestart:
+    #         print("Some thing wrong, app is restarting")
+    #         try:
+    #             loc=pag.locateOnScreen("IconApp.png")
+    #             pag.doubleClick(loc)
+    #             time.sleep(25)
+    #             pag.press(myPass[0])
+    #             time.sleep(2)
+    #             pag.press(myPass[1])
+    #             time.sleep(2)
+    #             pag.press(myPass[2])
+    #             time.sleep(2)
+    #             pag.press(myPass[3])
+    #             time.sleep(2)
+    #             pag.press(myPass[4])
+    #             time.sleep(2)
+    #             pag.press(myPass[5])
+    #             time.sleep(20)
+    #         except pag.ImageNotFoundException:
+    #             print("Sorry, Image cannot be recognized")
+    # time.sleep(timeCheck)
